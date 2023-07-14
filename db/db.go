0610b395f2a991e5027c41a2d72fd922083bd1c4
MRandyAnugerah <improud.ann@gmail.com>
2023-07-14 20:50:15 +0800

    add configuration for connection to SQL database

---
+++ db/db.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"database/sql"
+	"prakerja/config"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+var err error
+
+func Init() {
+	conf := config.GetConfig()
+	connectionString := conf.DB_USERNAME + ":" + conf.DB_PASSWORD + "@tcp(" + conf.DB_HOST + ":" + conf.DB_PORT + ")/" + conf.DB_NAME
+	db, err := sql.Open("mysql", connectionString)
+	if err != nil {
+		panic("connectionString Error ...")
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic("DNS Invalid ...")
+	}
+}
+
+func CreateCon() *sql.DB {
+	return db
+}
