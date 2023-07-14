54fc0fc3a3864d83ea8644eb99691cfdf2f4fdeb
MRandyAnugerah <improud.ann@gmail.com>
2023-07-14 22:32:22 +0800

    add pegawai's model and controller

--- db/db.go
+++ db/db.go
@@ -12,15 +12,17 @@ var err error
 
 func Init() {
 	conf := config.GetConfig()
+
 	connectionString := conf.DB_USERNAME + ":" + conf.DB_PASSWORD + "@tcp(" + conf.DB_HOST + ":" + conf.DB_PORT + ")/" + conf.DB_NAME
-	db, err := sql.Open("mysql", connectionString)
+
+	db, err = sql.Open("mysql", connectionString)
 	if err != nil {
-		panic("connectionString Error ...")
+		panic("connectionString error..")
 	}
 
 	err = db.Ping()
 	if err != nil {
-		panic("DNS Invalid ...")
+		panic("DSN Invalid")
 	}
 }
 
