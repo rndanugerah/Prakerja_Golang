d32efde28f486bffa24db562e8e3fcb8fe7b7fe0
MRandyAnugerah <improud.ann@gmail.com>
2023-07-14 23:06:55 +0800

    add POST Method to receive input data

--- models/pegawai.model.go
+++ models/pegawai.model.go
@@ -44,3 +44,34 @@ func FetchAllPegawai() (Response, error) {
 
 	return res, nil
 }
+
+func StorePegawai(nama string, email string, telepon string) (Response, error) {
+	var res Response
+
+	con := db.CreateCon()
+
+	sqlStatement := "INSERT pegawai (nama, email, telepon) VALUES (?, ?, ?)"
+
+	stmt, err := con.Prepare(sqlStatement)
+	if err != nil {
+		return res, err
+	}
+
+	result, err := stmt.Exec(nama, email, telepon)
+	if err != nil {
+		return res, err
+	}
+
+	lastInsertedId, err := result.LastInsertId()
+	if err != nil {
+		return res, err
+	}
+
+	res.Status = http.StatusOK
+	res.Message = "Success"
+	res.Data = map[string]int64{
+		"last_inserted_id": lastInsertedId,
+	}
+
+	return res, nil
+}
