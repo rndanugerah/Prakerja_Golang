7f84d755994ff04720484a0de534ecb831a02caa
MRandyAnugerah <improud.ann@gmail.com>
2023-07-14 23:18:17 +0800

    PUT Method For Changing Data

--- models/pegawai.model.go
+++ models/pegawai.model.go
@@ -75,3 +75,34 @@ func StorePegawai(nama string, email string, telepon string) (Response, error) {
 
 	return res, nil
 }
+
+func UpdatePegawai(id int, nama string, email string, telepon string) (Response, error) {
+	var res Response
+
+	con := db.CreateCon()
+
+	sqlStatement := "UPDATE pegawai SET nama = ?, email = ?, telepon = ? WHERE id = ?"
+
+	stmt, err := con.Prepare(sqlStatement)
+	if err != nil {
+		return res, err
+	}
+
+	result, err := stmt.Exec(nama, email, telepon, id)
+	if err != nil {
+		return res, err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return res, err
+	}
+
+	res.Status = http.StatusOK
+	res.Message = "Success"
+	res.Data = map[string]int64{
+		"rows_affected": rowsAffected,
+	}
+
+	return res, nil
+}
